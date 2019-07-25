abe3ff10cf89a3920c51d5d2ddabea10a07a7ac3
Simon Baudart <s.baudart@wavenet.be>
2019-07-25 10:06:49 +0200

    Add the CSP

--- Funcky.Security.CameraCloudCenter/Startup.cs
+++ Funcky.Security.CameraCloudCenter/Startup.cs
@@ -13,12 +13,15 @@ namespace Funcky.Security.CameraCloudCenter
 
     using AspNetCoreRateLimit;
 
-    using Funcky.Security.CameraCloudCenter.Core.Configuration;
-    using Funcky.Security.CameraCloudCenter.Core.Exceptions;
-    using Funcky.Security.CameraCloudCenter.Jobs;
+    using Core.Configuration;
+    using Core.Exceptions;
 
     using Hangfire;
     using Hangfire.MemoryStorage;
+
+    using Jobs;
+
+    using Joonasw.AspNetCore.SecurityHeaders;
 
     using Microsoft.AspNetCore.Authentication.Cookies;
     using Microsoft.AspNetCore.Builder;
@@ -29,7 +32,7 @@ namespace Funcky.Security.CameraCloudCenter
 
     using Newtonsoft.Json;
 
-    using GlobalConfiguration = Funcky.Security.CameraCloudCenter.Core.Configuration.GlobalConfiguration;
+    using GlobalConfiguration = Core.Configuration.GlobalConfiguration;
 
     /// <summary>
     /// Manage the application startup
@@ -37,7 +40,7 @@ namespace Funcky.Security.CameraCloudCenter
     public class Startup
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// Initializes a new instance of the <see cref="Startup" /> class.
         /// </summary>
         /// <param name="configuration">The configuration.</param>
         public Startup(IConfiguration configuration)
@@ -52,7 +55,7 @@ namespace Funcky.Security.CameraCloudCenter
         /// <value>
         /// The configuration.
         /// </value>
-        public IConfiguration Configuration { get; private set; }
+        private IConfiguration Configuration { get; }
 
         /// <summary>
         /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,14 +78,9 @@ namespace Funcky.Security.CameraCloudCenter
 
             GlobalConfiguration.Instance = JsonConvert.DeserializeObject<GlobalConfiguration>(File.ReadAllText(configurationFilePath));
 
-            this.StartHangfire(app);
-
-            app.UseIpRateLimiting();
-
-            app.UseAuthentication();
-            app.UseMvc();
-
-            app.UseStaticFiles();
+            this.ConfigureHangfire(app);
+            this.ConfigureSecurity(app);
+            this.ConfigureWeb(app);
         }
 
         /// <summary>
@@ -95,30 +93,73 @@ namespace Funcky.Security.CameraCloudCenter
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(
                     options =>
+                    {
+                        options.Events.OnRedirectToLogin = context =>
                         {
-                            options.Events.OnRedirectToLogin = context =>
-                                {
-                                    context.Response.StatusCode = 401;
-                                    return Task.CompletedTask;
-                                };
-                        });
+                            context.Response.StatusCode = 401;
+                            return Task.CompletedTask;
+                        };
+                    });
 
             services.AddMemoryCache();
             services.AddOptions();
             services.AddHttpContextAccessor();
 
-            this.ConfigureIpRateLimit(services);
+            this.ConfigureServiceIpRateLimit(services);
+            this.ConfigureServiceSecurity(services);
 
             services.AddMvc();
 
-            this.ConfigureHangfire(services);
+            this.ConfigureServiceHangfire(services);
+        }
+
+        /// <summary>
+        /// Starts the hangfire.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <exception cref="System.ApplicationException">The configuration file {configurationFilePath} does not exists</exception>
+        private void ConfigureHangfire(IApplicationBuilder app)
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions {Authorization = new[] {new EveryOneAuthorization()}});
+
+            this.EnsureConfiguration(GlobalConfiguration.Instance.Configurations);
+
+            foreach (var configuration in GlobalConfiguration.Instance.Configurations)
+            {
+                RecurringJob.AddOrUpdate<CameraInputProcessor>($"PROCESS INPUT : {configuration.Key}", x => x.Process(configuration), Cron.Minutely(), TimeZoneInfo.Utc);
+                RecurringJob.AddOrUpdate<CameraOutputCleanup>($"PROCESS CLEAN : {configuration.Key}", x => x.Process(configuration), Cron.Hourly(), TimeZoneInfo.Utc);
+            }
+        }
+
+        /// <summary>
+        /// Configure all things related to security for the application
+        /// </summary>
+        /// <param name="app">The application builder used to configure</param>
+        private void ConfigureSecurity(IApplicationBuilder app)
+        {
+            app.UseCsp(csp =>
+            {
+                csp.ByDefaultAllow.FromSelf();
+                csp.AllowScripts.FromSelf()
+                    .From("'sha256-ox311wuoHk7Vrx1uEa8PnZt34XhOUHuWdkjGrJLOmPM='")
+                    .From("code.jquery.com")
+                    .From("unpkg.com");
+                csp.AllowStyles.FromSelf()
+                    .From("fonts.googleapis.com")
+                    .From("unpkg.com");
+                csp.AllowFonts.FromSelf()
+                    .From("data:")
+                    .From("fonts.gstatic.com");
+                csp.AllowImages.FromSelf()
+                    .From("data:");
+            });
         }
 
         /// <summary>
         /// Configures the hangfire.
         /// </summary>
         /// <param name="services">The services.</param>
-        private void ConfigureHangfire(IServiceCollection services)
+        private void ConfigureServiceHangfire(IServiceCollection services)
         {
             services.AddHangfire(x => x.UseMemoryStorage());
             services.AddHangfireServer();
@@ -128,7 +169,7 @@ namespace Funcky.Security.CameraCloudCenter
         /// Configures the ip rate limit.
         /// </summary>
         /// <param name="services">The services.</param>
-        private void ConfigureIpRateLimit(IServiceCollection services)
+        private void ConfigureServiceIpRateLimit(IServiceCollection services)
         {
             services.Configure<IpRateLimitOptions>(this.Configuration.GetSection("IpRateLimiting"));
             services.Configure<IpRateLimitPolicies>(this.Configuration.GetSection("IpRateLimitPolicies"));
@@ -138,6 +179,29 @@ namespace Funcky.Security.CameraCloudCenter
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        }
+
+        /// <summary>
+        /// Configure all services related to security
+        /// </summary>
+        /// <param name="services"></param>
+        private void ConfigureServiceSecurity(IServiceCollection services)
+        {
+            services.AddCsp();
+        }
+
+        /// <summary>
+        /// Configure all part of the web
+        /// </summary>
+        /// <param name="app">The application builder</param>
+        private void ConfigureWeb(IApplicationBuilder app)
+        {
+            app.UseIpRateLimiting();
+
+            app.UseAuthentication();
+            app.UseMvc();
+
+            app.UseStaticFiles();
         }
 
         /// <summary>
@@ -157,24 +221,6 @@ namespace Funcky.Security.CameraCloudCenter
             if (configurations.Any(x => configurations.Count(c => c.Key == x.Key) != 1))
             {
                 throw new ConfigurationException("All camera configuration must have a unique key");
-            }
-        }
-
-        /// <summary>
-        /// Starts the hangfire.
-        /// </summary>
-        /// <param name="app">The application.</param>
-        /// <exception cref="System.ApplicationException">The configuration file {configurationFilePath} does not exists</exception>
-        private void StartHangfire(IApplicationBuilder app)
-        {
-            app.UseHangfireDashboard("/hangfire", new DashboardOptions { Authorization = new[] { new EveryOneAuthorization() } });
-
-            this.EnsureConfiguration(GlobalConfiguration.Instance.Configurations);
-
-            foreach (var configuration in GlobalConfiguration.Instance.Configurations)
-            {
-                RecurringJob.AddOrUpdate<CameraInputProcessor>($"PROCESS INPUT : {configuration.Key}", x => x.Process(configuration), Cron.Minutely(), TimeZoneInfo.Utc);
-                RecurringJob.AddOrUpdate<CameraOutputCleanup>($"PROCESS CLEAN : {configuration.Key}", x => x.Process(configuration), Cron.Hourly(), TimeZoneInfo.Utc);
             }
         }
     }
