f4e7d858da0b07474e9f6c1bcefa21c589a3e0e4
Simon Baudart <s.baudart@wavenet.be>
2019-07-25 10:16:37 +0200

    Configure Kestrel to don't add the Server header

--- Funcky.Security.CameraCloudCenter/Program.cs
+++ Funcky.Security.CameraCloudCenter/Program.cs
@@ -18,22 +18,24 @@ namespace Funcky.Security.CameraCloudCenter
     public class Program
     {
         /// <summary>
-        /// Creates the web host builder.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
-        /// <returns>The web host builder for this application</returns>
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
-        }
-
-        /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Creates the web host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>The web host builder for this application</returns>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel(k => { k.AddServerHeader = false; })
+                .UseStartup<Startup>();
         }
     }
 }
